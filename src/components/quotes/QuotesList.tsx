878fe4b93543d0de999b36572e6d7301e0513d64
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-08-03 16:52:20 +0100

    refactor: refactored code for the quoteslist component, to include usecallback for pagination function, and provide a better ux when transitioning between pages, with the placeholder and  keepPreviousData from usequery

--- src/components/quotes/QuotesList.tsx
+++ src/components/quotes/QuotesList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useCallback, useState } from 'react';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
 
 import { fetchData } from '@/utilities/fetchData/fetchData';
 
@@ -15,29 +15,37 @@ import HeadingOne from '../text/HeadingOne';
 const QuotesList = () => {
   const [page, setPage] = useState<number>(1);
 
-  const url = `https://api.quotable.io/quotes?page=${page}&sortBy=content`;
+  const buildUrl = (page: number) =>
+    `https://api.quotable.io/quotes?page=${page}&sortBy=content`;
 
-  const { data: quotes, isLoading } = useQuery({
-    queryKey: ['quotes', url],
-    queryFn: async () => fetchData(url),
+  const {
+    data: quotes,
+    isPending,
+    isFetching,
+  } = useQuery({
+    queryKey: ['quotes', buildUrl(page)],
+    queryFn: async () => fetchData(buildUrl(page)),
+    placeholderData: keepPreviousData,
   });
 
-  const handlePagination = (pageCount: number) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-
-    // give time for scrollToTop animation to complete
-    const timeout = setTimeout(() => {
+  const handlePagination = useCallback(
+    (pageCount: number) => {
+      // optimistic ui update (atleast I think that is what this is doing)
       setPage(pageCount);
-      return clearTimeout(timeout);
-    }, 1000);
-  };
+
+      requestAnimationFrame(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
+    },
+    [page]
+  );
 
   return (
     <div className='space-y-6 lg:space-y-10'>
       <HeadingOne>{`"IN THE WORDS OF THE WISE"`}</HeadingOne>
 
       <div className='grid gap-6 md:grid-cols-2 md:gap-10 lg:grid-cols-3'>
-        {isLoading ? (
+        {isPending ? (
           <LazyQuotes />
         ) : (
           quotes.results.map((quote: Quote) => (
@@ -47,7 +55,7 @@ const QuotesList = () => {
       </div>
 
       <Pagination
-        loading={isLoading}
+        loading={isFetching}
         page={page}
         handlePagination={handlePagination}
       />
